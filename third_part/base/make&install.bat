b5b4252bc4a9dd47fdf850181abc8caa5950c64f
bdgboss <bdgboss12@gmail.com>
2018-12-23 00:13:28 +0800

    添加多版本文件

--- third_part/base/make&install.bat
+++ third_part/base/make&install.bat
@@ -1,8 +1,19 @@
 SET filedir=%~dp0
 mkdir sln
 cd sln
-cmake ..
+cmake -G "Visual Studio 14 2015" ..
 cmake --build . --target install --clean-first --config Debug
+cmake --build . --target install --clean-first --config Release
 cd ..
 rmdir /s /q sln
+
+mkdir sln64
+cd sln64
+cmake -G "Visual Studio 14 2015 Win64" ..
+cmake --build . --target install --clean-first --config Debug
+cmake --build . --target install --clean-first --config Release
+cd ..
+rmdir /s /q sln64
+
 pause
+
