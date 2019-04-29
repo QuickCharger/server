22cbd892356753c89b328ece546a5486dc0624a5
BaDenggao <479303780@qq.com>
2019-04-29 17:51:09 +0800

    规范化编译脚本

--- third_part/protobuf-2.6.1/build.bat
+++ third_part/protobuf-2.6.1/build.bat
@@ -1,21 +1,36 @@
+IF DEFINED VS140COMNTOOLS (
+  SET VCVARSALL="%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
+) ELSE (
+  FOR /F "tokens=1,2,*" %%I IN ('REG QUERY HKLM\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7 /v 15.0 ^| FIND "15.0"') DO SET VCVARSALL="%%~KVC\Auxiliary\Build\vcvarsall.bat"
+)
+
+IF NOT DEFINED VCVARSALL (
+  ECHO Can not find VC2015 or VC2017 installed!
+  GOTO ERROR
+)
+
+CALL %VCVARSALL% amd64
+
 SET PROTOBUF_DIR=%~dp0
 
 RD /S /Q lib
 
-SET DEVENV="%VS140COMNTOOLS%..\IDE\devenv.com"
-
-rem win32 debug
-%DEVENV% ./vsprojects/protobuf.sln /ReBuild "Debug|Win32" /Project libprotobuf
+@rem win32 debug
+devenv ./vsprojects/protobuf.sln /ReBuild "Debug|Win32" /Project libprotobuf
 XCOPY /f vsprojects\Debug\libprotobuf.lib lib\windows_x86_32_msvc14\Debug\
 
-rem win32 release
-%DEVENV% ./vsprojects/protobuf.sln /ReBuild "Release|Win32" /Project libprotobuf
+@rem win32 release
+devenv ./vsprojects/protobuf.sln /ReBuild "Release|Win32" /Project libprotobuf
 XCOPY /f vsprojects\Release\libprotobuf.lib lib\windows_x86_32_msvc14\Release\
 
-rem win64 debug
-%DEVENV% ./vsprojects/protobuf.sln /ReBuild "Debug|x64" /Project libprotobuf
+@rem win64 debug
+devenv ./vsprojects/protobuf.sln /ReBuild "Debug|x64" /Project libprotobuf
 XCOPY /f vsprojects\x64\Debug\libprotobuf.lib lib\windows_x86_64_msvc14\Debug\
 
-rem win64 release
-%DEVENV% ./vsprojects/protobuf.sln /ReBuild "Release|x64" /Project libprotobuf
+@rem win64 release
+devenv ./vsprojects/protobuf.sln /ReBuild "Release|x64" /Project libprotobuf
 XCOPY /f vsprojects\x64\Release\libprotobuf.lib lib\windows_x86_64_msvc14\Release\
+
+GOTO :EOF
+
+:ERROR
